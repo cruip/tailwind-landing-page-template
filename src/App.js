086c3e2694f5384edbe3c2769e5cbaf13cfeb7aa
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-05 01:14:01 -0500

    playing with tailwind template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { useEffect } from 'react';
-import {
-  Switch,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 
 import './css/style.scss';
 
@@ -11,48 +7,35 @@ import AOS from 'aos';
 import { focusHandling } from 'cruip-js-toolkit';
 
 import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import ResetPassword from './pages/ResetPassword';
 
 function App() {
+	const location = useLocation();
 
-  const location = useLocation();
+	useEffect(() => {
+		AOS.init({
+			once: true,
+			disable: 'phone',
+			duration: 700,
+			easing: 'ease-out-cubic',
+		});
+	});
 
-  useEffect(() => {
-    AOS.init({
-      once: true,
-      disable: 'phone',
-      duration: 700,
-      easing: 'ease-out-cubic',
-    });
-  });
+	useEffect(() => {
+		document.querySelector('html').style.scrollBehavior = 'auto';
+		window.scroll({ top: 0 });
+		document.querySelector('html').style.scrollBehavior = '';
+		focusHandling('outline');
+	}, [location.pathname]); // triggered on route change
 
-  useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
-    focusHandling('outline');
-  }, [location.pathname]); // triggered on route change
-
-  return (
-    <>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/signin">
-          <SignIn />
-        </Route>
-        <Route path="/signup">
-          <SignUp />
-        </Route>
-        <Route path="/reset-password">
-          <ResetPassword />
-        </Route>
-      </Switch>
-    </>
-  );
+	return (
+		<>
+			<Switch>
+				<Route exact path='/'>
+					<Home />
+				</Route>
+			</Switch>
+		</>
+	);
 }
 
 export default App;
