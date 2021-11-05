e511ca8e976e0e496d76a074c76d44d32c0f69ba
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-05 01:39:23 -0500

    commiting changes for NASA daily photo

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -1,66 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 
 function Header() {
-	const [top, setTop] = useState(true);
-
-	// detect whether user has scrolled the page down by 10px
-	useEffect(() => {
-		const scrollHandler = () => {
-			window.pageYOffset > 10 ? setTop(false) : setTop(true);
-		};
-		window.addEventListener('scroll', scrollHandler);
-		return () => window.removeEventListener('scroll', scrollHandler);
-	}, [top]);
-
-	return (
-		<header
-			className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${
-				!top && 'bg-white blur shadow-lg'
-			}`}
-		>
-			<div className='max-w-6xl mx-auto px-5 sm:px-6'>
-				<div className='flex items-center justify-between h-16 md:h-20'>
-					{/* Site branding */}
-
-					<div className='flex-shrink-0 mr-4'>
-						{/* Logo */}
-						<Link to='/' className='block' aria-label='Cruip'>
-							<svg
-								className='w-8 h-8'
-								viewBox='0 0 32 32'
-								xmlns='http://www.w3.org/2000/svg'
-							>
-								<defs>
-									<radialGradient
-										cx='21.152%'
-										cy='86.063%'
-										fx='21.152%'
-										fy='86.063%'
-										r='79.941%'
-										id='header-logo'
-									>
-										<stop stopColor='#4FD1C5' offset='0%' />
-										<stop stopColor='#81E6D9' offset='25.871%' />
-										<stop stopColor='#338CF5' offset='100%' />
-									</radialGradient>
-								</defs>
-								<rect
-									width='32'
-									height='32'
-									rx='16'
-									fill='url(#header-logo)'
-									fillRule='nonzero'
-								/>
-							</svg>
-						</Link>
-					</div>
-
-					{/* Site navigation */}
-				</div>
-			</div>
-		</header>
-	);
+	return <header></header>;
 }
 
 export default Header;
--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
-import Modal from '../utils/Modal';
 
 function HeroHome() {
-	const [videoModalOpen, setVideoModalOpen] = useState(false);
-
 	return (
 		<section className='relative'>
 			{/* Illustration behind hero content */}
@@ -39,33 +36,32 @@ function HeroHome() {
 
 			<div className='max-w-6xl mx-auto px-4 sm:px-6'>
 				{/* Hero content */}
-				<div className='pt-32 pb-12 md:pt-40 md:pb-20'>
-					{/* Section header */}
-					<div className='text-center pb-12 md:pb-16'>
-						<h1
-							className='text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4'
+
+				{/* Section header */}
+				<div className='text-center pb-12 md:pb-16'>
+					<h1
+						className='text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4'
+						data-aos='zoom-y-out'
+					>
+						Images
+						<span className='bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400'>
+							Daily
+						</span>
+					</h1>
+					<div className='max-w-3xl mx-auto'>
+						<p
+							className='text-xl text-gray-600 mb-8'
 							data-aos='zoom-y-out'
+							data-aos-delay='150'
 						>
-							Images
-							<span className='bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400'>
-								Daily
-							</span>
-						</h1>
-						<div className='max-w-3xl mx-auto'>
-							<p
-								className='text-xl text-gray-600 mb-8'
-								data-aos='zoom-y-out'
-								data-aos-delay='150'
-							>
-								Thank you for visiting our site! Enjoy our daily NASA
-								image below.
-							</p>
-							<div
-								className='max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center'
-								data-aos='zoom-y-out'
-								data-aos-delay='300'
-							></div>
-						</div>
+							Thank you for visiting our site! Enjoy our daily NASA image
+							below.
+						</p>
+						<div
+							className='max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center'
+							data-aos='zoom-y-out'
+							data-aos-delay='300'
+						></div>
 					</div>
 
 					{/* Hero image */}
@@ -162,45 +158,7 @@ function HeroHome() {
 									</g>
 								</svg>
 							</div>
-							<button
-								className='absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg'
-								onClick={(e) => {
-									e.preventDefault();
-									e.stopPropagation();
-									setVideoModalOpen(true);
-								}}
-								aria-controls='modal'
-							>
-								<svg
-									className='w-6 h-6 fill-current text-gray-400 group-hover:text-blue-600 flex-shrink-0'
-									viewBox='0 0 24 24'
-									xmlns='http://www.w3.org/2000/svg'
-								>
-									<path d='M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z' />
-									<path d='M10 17l6-5-6-5z' />
-								</svg>
-								<span className='ml-3'>
-									Watch the full video (2 min)
-								</span>
-							</button>
 						</div>
-
-						{/* Modal */}
-						<Modal
-							id='modal'
-							ariaLabel='modal-headline'
-							show={videoModalOpen}
-							handleClose={() => setVideoModalOpen(false)}
-						>
-							<div className='relative pb-9/16'>
-								<iframe
-									className='absolute w-full h-full'
-									src='https://player.vimeo.com/video/174002812'
-									title='Video'
-									allowFullScreen
-								></iframe>
-							</div>
-						</Modal>
 					</div>
 				</div>
 			</div>
