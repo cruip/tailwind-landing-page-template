c3827bd7f8593d3299f70cd723af06a2978528ad
Paipo Tang <paipo.tang@doordash.com>
2022-06-12 12:03:34 +0900

    Add no mobile icon

--- src/partials/CouplerHero.jsx
+++ src/partials/CouplerHero.jsx
@@ -40,13 +40,64 @@ function CouplerHero() {
                 data-aos="zoom-y-out"
               >
                 <p
-                  className="text-sm md:text-md mb-8 text-gray-600 font-inter"
+                  className="text-sm md:text-md text-gray-600 font-inter mb-2"
                   data-aos="zoom-y-out"
                   data-aos-delay="150"
                 >
                   Please fill your email adrees and click the generate button.
                   And we well generate the code for your space.
                 </p>
+                <p
+                  className="mb-2 text-sm text-gray-600"
+                  data-aos="zoom-y-out"
+                  data-aos-delay="150"
+                >
+                  *Please use laptop to try our platform, mobile doesn’t work at
+                  the moment.
+                </p>
+                <div className="flex justify-center mb-8">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M2 19H22M4 7C4 6.46957 4.21071 5.96086 4.58579 5.58579C4.96086 5.21071 5.46957 5 6 5H18C18.5304 5 19.0391 5.21071 19.4142 5.58579C19.7893 5.96086 20 6.46957 20 7V16H4V7Z"
+                      stroke="#4B4B4B"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M16 2H8C6.89543 2 6 2.89543 6 4V20C6 21.1046 6.89543 22 8 22H16C17.1046 22 18 21.1046 18 20V4C18 2.89543 17.1046 2 16 2Z"
+                      stroke="#4B4B4B"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M11.9492 18H12.0492"
+                      stroke="#4B4B4B"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <line
+                      x1="2.09957"
+                      y1="21.6464"
+                      x2="21.2925"
+                      y2="2.45355"
+                      stroke="#4B4B4B"
+                    />
+                  </svg>
+                </div>
                 <ApplicationForm />
               </div>
             </div>
