3c09c5975e211120fb38e79615acafb2f987fc97
Paipo Tang <paipo.tang@doordash.com>
2022-06-29 16:28:51 +0900

    Add ogp using react-helmet

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ResetPassword from "./pages/ResetPassword";
 import Coupler from "./pages/Coupler";
+import { Helmet } from "react-helmet";
 
 function App() {
   const location = useLocation();
@@ -31,6 +32,19 @@ function App() {
 
   return (
     <>
+      <Helmet>
+        <meta property="og:title" content="MashiMashi" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/wesley-dowel.appspot.com/o/static%2Flogo-bk.png?alt=media&token=51b87bf4-c3de-4584-b4c3-36bd3f28fb41"
+        />
+        <meta property="og:url" content="https://www.creativemashimashi.com/" />
+        <meta
+          property="og:description"
+          content="We help companies, leaders build and improve team cohesion for a greater business efficiency."
+        />
+      </Helmet>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
--- src/pages/Coupler.jsx
+++ src/pages/Coupler.jsx
@@ -7,24 +7,50 @@ import CreateProfile from "../partials/CreateProfile";
 import FindRightPeople from "../partials/FindRightPeople";
 import CheckProfile from "../partials/CheckProfile";
 import TryItOut from "../partials/TryItOut";
+import { Helmet } from "react-helmet";
 
 function Coupler() {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-      {/*  Site header */}
-      <Header />
-      {/*  Page content */}
-      <main className="flex-grow font-coupler">
-        <CouplerHero />
-        <PickupRole />
-        <CreateProfile />
-        <FindRightPeople />
-        <CheckProfile />
-        <TryItOut />
-      </main>
-      {/*  Site footer */}
-      <Footer />
-    </div>
+    <>
+      <Helmet>
+        <meta
+          name="description"
+          content="A new platform we created for information providers and receivers."
+        />
+        <meta
+          property="og:title"
+          content="Coupler - Create valuable connection"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/wesley-dowel.appspot.com/o/static%2Fcoupler-ogp.png?alt=media&token=9263dd76-973c-46c4-872d-0bb195277f1c"
+        />
+        <meta
+          property="og:url"
+          content="https://www.creativemashimashi.com/platform"
+        />
+        <meta
+          property="og:description"
+          content="A new platform we created for information providers and receivers."
+        />
+      </Helmet>
+      <div className="flex flex-col min-h-screen overflow-hidden">
+        {/*  Site header */}
+        <Header />
+        {/*  Page content */}
+        <main className="flex-grow font-coupler">
+          <CouplerHero />
+          <PickupRole />
+          <CreateProfile />
+          <FindRightPeople />
+          <CheckProfile />
+          <TryItOut />
+        </main>
+        {/*  Site footer */}
+        <Footer />
+      </div>
+    </>
   );
 }
 
