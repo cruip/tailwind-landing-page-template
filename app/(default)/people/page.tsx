bccf0b51f7d8d02e72de186fa7cec5e6f84671e2
pier <piergiuseppe.mallozzi@gmail.com>
2023-04-19 19:47:01 -0700

    new pages

---
+++ app/(default)/people/page.tsx
@@ -0,0 +1,23 @@
+export const metadata = {
+  title: "Sign In - Simple",
+  description: "Page description",
+};
+
+import Link from "next/link";
+
+export default function SignIn() {
+  return (
+    <section className="bg-gradient-to-b from-gray-100 to-white">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+          {/* Page header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h1 className="h1">People</h1>
+          </div>
+
+          <div className="max-w-sm mx-auto text-center">Content</div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -38,30 +38,28 @@ export default function Header() {
           <nav className="hidden md:flex md:grow">
             {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
-              {/* <li>
+              <li>
                 <Link
-                  href="/cont"
+                  href="/more"
                   className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
                 >
-                  Demo
+                  Learn More
                 </Link>
-              </li> */}
+              </li>
+              <li>
+                <Link
+                  href="/people"
+                  className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
+                >
+                  People
+                </Link>
+              </li>
               <li>
                 <Link
                   href="/demo"
                   className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3"
                 >
                   <span>Demo</span>
-                  <svg
-                    className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1"
-                    viewBox="0 0 12 12"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                      fillRule="nonzero"
-                    />
-                  </svg>
                 </Link>
               </li>
             </ul>
