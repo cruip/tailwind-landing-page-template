c3dba11d990f101a4244be3d03618af506e266e3
Guillaume Lethuillier <66550865+glethuillier@users.noreply.github.com>
2023-04-21 20:56:20 +0200

    Landing page: restore header

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import Logo from "./logo";
 import Dropdown from "@/components/utils/dropdown";
+import MobileMenu from "./mobile-menu";
 
 export default function Header() {
   const [top, setTop] = useState<boolean>(true);
@@ -26,6 +27,46 @@ export default function Header() {
         !top ? "bg-white backdrop-blur-sm shadow-lg" : ""
       }`}
     >
+
+<div className="max-w-6xl mx-auto px-5 sm:px-6">
+        <div className="flex items-center justify-between h-16 md:h-20">
+          {/* Site branding */}
+  
+
+          {/* Desktop navigation */}
+          <nav className="hidden md:flex md:grow">
+            {/* Desktop sign in links */}
+            <ul className="flex grow justify-end flex-wrap items-center">
+              <h4>
+                <Link
+                  href="/more"
+                  className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
+                >
+                  Learn More
+                </Link>
+              </h4>
+              <h4>
+                <Link
+                  href="/people"
+                  className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
+                >
+                  People
+                </Link>
+              </h4>
+              <h4>
+                <Link
+                  href="/demo"
+                  className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3"
+                >
+                  <span>Demo</span>
+                </Link>
+              </h4>
+            </ul>
+          </nav>
+
+          <MobileMenu />
+        </div>
+      </div>
 
     </header>
   );
