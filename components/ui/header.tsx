59043b1c32f473d005031d40d6abf8babadffb79
Guillaume Lethuillier <66550865+glethuillier@users.noreply.github.com>
2023-04-21 21:06:03 +0200

    Restore top left logo

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -31,7 +31,9 @@ export default function Header() {
 <div className="max-w-6xl mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
           {/* Site branding */}
-  
+          <div className="shrink-0 mr-4">
+            <Logo />
+          </div>
 
           {/* Desktop navigation */}
           <nav className="hidden md:flex md:grow">
