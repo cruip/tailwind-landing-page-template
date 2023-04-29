12c5b48390231fc24f171ed54b83a3f72ceb5aec
Guillaume Lethuillier <66550865+glethuillier@users.noreply.github.com>
2023-04-29 08:12:48 +0200

    Update logo

--- components/fact-fortress.tsx
+++ components/fact-fortress.tsx
@@ -25,11 +25,10 @@ export default function Hero() {
               justifyContent: "center",
             }}
           >
-            <Image className="mx-auto" src={Logo} width={300} priority alt="Hero" />
+            <Image className="mx-auto" src={Logo} width={600} priority alt="Hero" />
           </div>
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Fact Fortress</h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">On-Chain Zero-Knowledge Proof Solution to Fact-Checking.</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
