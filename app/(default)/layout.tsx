46ba246f349a1258e856a94bb9f1b015a2727039
Lienid <0xLienid@protonmail.com>
2023-10-07 23:16:40 -0500

    Set up diff pages

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -29,8 +29,6 @@ export default function DefaultLayout({
         {children}
 
       </main>
-
-      <Footer />
     </>
   )
 }
---
+++ app/pricing/page.tsx
@@ -0,0 +1,9 @@
+import Pricing from "@/components/pricing";
+
+export default function PricingPage() {
+    return (
+        <>
+            <Pricing />
+        </>
+    );
+}
