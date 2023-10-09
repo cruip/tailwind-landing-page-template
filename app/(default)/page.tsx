fd5dde0e10e932a05ce554e776b81199c67d01ba
Lienid <0xLienid@protonmail.com>
2023-10-09 09:59:58 -0500

    Shift location of meta tags

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -8,25 +8,10 @@ import Features from '@/components/features'
 import FeaturesBlocks from '@/components/features-blocks'
 import Testimonials from '@/components/testimonials'
 import Newsletter from '@/components/newsletter'
-import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
-      <Head>
-        {/* Open Graph */}
-        <meta property="og:title" content="OneStudy" />
-        <meta property="og:description" content="More than flashcards. Your comprehensive study companion." />
-        <meta property="og:image" content="/images/OneStudy-social.png" />
-        <meta property="og:type" content="website" />
-
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={"OneStudy"} />
-        <meta name="twitter:description" content="More than flashcards. Your comprehensive study companion." />
-        <meta name="twitter:image" content="/images/OneStudy-social.png"></meta>
-        <link rel="icon" href="public/favicon.ico" />
-      </Head>
       <Hero />
       <Features />
       <FeaturesBlocks />      
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google'
 import Header from '@/components/ui/header'
 import Banner from '@/components/banner'
 import Footer from '@/components/ui/footer'
+import Head from 'next/head'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -26,6 +27,20 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} font-inter antialiased bg-white text-gray-900 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
+          <Head>
+            {/* Open Graph */}
+            <meta property="og:title" content="OneStudy" />
+            <meta property="og:description" content="More than flashcards. Your comprehensive study companion." />
+            <meta property="og:image" content="/images/OneStudy-social.png" />
+            <meta property="og:type" content="website" />
+
+            {/* Twitter */}
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content={"OneStudy"} />
+            <meta name="twitter:description" content="More than flashcards. Your comprehensive study companion." />
+            <meta name="twitter:image" content="/images/OneStudy-social.png"></meta>
+            <link rel="icon" href="public/favicon.ico" />
+          </Head>
           <Header />
           {children}   
           <Footer />       
