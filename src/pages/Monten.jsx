2c5bb0ac4e58417d068b000f10c75ae3bf356260
Paipo Tang <paipo.tang@wolt.com>
2023-11-05 19:51:19 +0900

    Update email handling

--- src/pages/Monten.jsx
+++ src/pages/Monten.jsx
@@ -4,6 +4,30 @@ import MontenLandingImage from "../images/monten-landing.png";
 import MontenHeader from '../partials/MontenHeader';
 
 function Monten() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const subscriberEmail = event.target.email
+    //call to the Netlify Function you created
+    
+    try {
+      await fetch(
+        `${process.env.URL}/.netlify/functions/emails/subscribed`,
+        {
+          headers: {
+            "netlify-emails-secret": process.env.NETLIFY_EMAILS_SECRET,
+          },
+          method: "POST",
+          body: JSON.stringify({
+            from: "ptang8@bus.illinois.edu",
+            to: subscriberEmail,
+            subject: "Great to have you on board!",
+          }),
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <Helmet>
@@ -37,7 +61,7 @@ function Monten() {
           <div className="max-w-3xl xl:mr-24">
             <h1 className="text-4xl md:text-6xl font-bold mb-14">悩んでいる時、 みんなの意見が気にする？</h1>
             <h2 className="text-xl mb-20">Monten は、好奇心の世界への扉を開き、あなたの興味をそそるものなら何でも、匿名で世間の声を探ります。決断する自信を求めている人も、単純に楽しい時間を求めている人も、Monten は無限の可能性への入り口です。</h2>
-            <form className="flex gap-3">
+            <form className="flex gap-3" onSubmit={handleSubmit}>
               <input
                 type="text"
                 className="w-full max-w-xs border border-gray-300 px-4 py-2 rounded-md"
