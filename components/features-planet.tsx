f7359b977175fcc2e62b566be98a46b1a9ca1783
Lynn <57570218+tangminhanh@users.noreply.github.com>
2024-07-11 15:31:17 +0800

    update about-me

--- components/features-planet.tsx
+++ components/features-planet.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from "next/image";
 import PlanetImg from "@/public/images/planet.png";
 import PlanetOverlayImg from "@/public/images/planet-overlay.svg";
@@ -5,8 +6,23 @@ import PlanetTagImg01 from "@/public/images/planet-tag-01.png";
 import PlanetTagImg02 from "@/public/images/planet-tag-02.png";
 import PlanetTagImg03 from "@/public/images/planet-tag-03.png";
 import PlanetTagImg04 from "@/public/images/planet-tag-04.png";
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function FeaturesPlanet() {
+
+  const [viewportHeight, setViewportHeight] = useState(0);
+  useEffect(() => {
+    
+  function handleResize() {
+      setViewportHeight(window.innerHeight);
+    }
+    handleResize(); // Initial call
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <section className="relative before:absolute before:inset-0 before:-z-20 before:bg-gray-900">
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
@@ -71,40 +87,137 @@ export default function FeaturesPlanet() {
         </div>
       </div>
     </div>
-    {/* Grid */}
-    <div className="grid grid-cols-3 md:gap-5 gap-2 text-center px-2 mx-auto">
+    {/* Grid + Skill*/}
+    <div className="flex flex-col">
+    {/*Grid */}
+    <div className="grid grid-cols-3 md:gap-5 gap-2 text-center px-2 mx-auto my-auto">
     
       <div className="group rounded-lg border border-transparent p-[0.5em] md:p-[1em] shadow-sm transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
         <div className="flex dark:drop-shadow-[0_0_0.8rem_#ffffff70] items-center">
-          <h3 className="font-semibold ml-[0.5em] text-xs sm:text-sm md:text-base bg-gradient-to-r bg-clip-text text-transparent from-pink-400 via-purple-500 to-indigo-500 animate-text">
+          <h3 className="font-semibold ml-[0.5em] text-xs sm:text-sm md:text-base bg-gradient-to-r bg-clip-text text-center text-transparent from-pink-400 via-purple-500 to-indigo-500 animate-text">
             Experience
           </h3>
         </div>
-        <p className="custom-font mt-2 rounded-xl px-[0.5em] text-sm py-[0.5em] md:px-[1.4em] md:py-[0.6em] md:text-lg text-center transition-all hover:text-black hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-indigo-500">
-          1 Year
+        <p className="custom-font mt-2 rounded-xl px-[0.2em] text-sm py-[0.5em] md:px-[1.4em] md:py-[0.6em] md:text-lg text-center transition-all hover:text-black hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-indigo-500">
+          1 <br/> Year
         </p>
       </div>
       <div className="group rounded-lg border border-transparent p-[0.5em] md:p-[1em] shadow-sm transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
         <div className="flex dark:drop-shadow-[0_0_0.8rem_#ffffff70] items-center">
-          <h3 className="font-semibold ml-[0.5em] text-xs sm:text-sm md:text-base bg-gradient-to-r bg-clip-text text-transparent from-pink-400 via-purple-500 to-indigo-500 animate-text">
+          <h3 className="font-semibold ml-[0.5em] text-xs sm:text-sm md:text-base bg-gradient-to-r bg-clip-text text-center text-transparent from-pink-400 via-purple-500 to-indigo-500 animate-text">
             Completed
           </h3>
         </div>
-        <p className="custom-font mt-2 rounded-xl px-[0.5em] py-[0.5em] text-sm md:px-[1.4em] md:py-[0.6em] md:text-lg text-center transition-all hover:text-black hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-indigo-500">
-          10+ Projects
+        <p className="custom-font mt-2 rounded-xl px-[0.2em] py-[0.5em] text-sm md:px-[1.4em] md:py-[0.6em] md:text-lg text-center transition-all hover:text-black hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-indigo-500">
+          10+<br/>Projects
         </p>
       </div>
       <div className="group rounded-lg border border-transparent p-[0.5em] md:p-[1em] shadow-sm transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
         <div className="flex dark:drop-shadow-[0_0_0.8rem_#ffffff70] items-center">
-          <h3 className="font-semibold ml-[0.5em] text-xs sm:text-sm md:text-base bg-gradient-to-r bg-clip-text text-transparent from-pink-400 via-purple-500 to-indigo-500 animate-text">
+          <h3 className="font-semibold ml-[0.5em] text-xs sm:text-sm md:text-base bg-gradient-to-r bg-clip-text text-center text-transparent from-pink-400 via-purple-500 to-indigo-500 animate-text">
             Worked in
           </h3>
         </div>
-        <p className="custom-font mt-2 rounded-xl px-[0.5em] py-[0.5em] text-sm md:px-[1.4em] md:py-[0.6em] md:text-lg text-center transition-all hover:text-black hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-indigo-500">
-          4+ Company
+        <p className="custom-font mt-2 rounded-xl px-[0.2em] py-[0.5em] text-sm md:px-[1.4em] md:py-[0.6em] md:text-lg text-center transition-all hover:text-black hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-indigo-500">
+          4+ <br/>Company
         </p>
       </div>
     
+  </div>
+  {/*Skills */}
+  <div>
+    {viewportHeight > 600 && (
+      <div className="mx-auto max-w-[30em] mt-[0.25em] px-[1.5em] sm:max-w-[30em] md:max-w-[34em] md:px-[2em] lg:px-[1em] sm:py-[1em] md:py-[0.5em]">
+            <h3 className="text-center text-[0.5em] md:text-[0.75em] font-semibold text-gray-500 uppercase">Works with different technologies</h3>
+             <div className="mx-auto mt-[1em] grid max-w-[20em] items-center gap-x-[1em] gap-y-[1em] sm:max-w-[24em] sm:gap-x-[1.5em] lg:mx-0 lg:max-w-none sm:grid-cols-8 grid-cols-4 sm:justify-center">
+
+          <img
+            className="max-h-[3em] w-full object-contain col-span-1"
+            src="https://res.cloudinary.com/dho2pmur7/image/upload/c_scale,w_300/v1682840456/image_2_misfnu.png"
+            alt="Javascript"
+          />
+          <img
+            className="max-h-[3em] w-full object-contain col-span-1"
+            src="https://res.cloudinary.com/dho2pmur7/image/upload/c_scale,w_300/v1682843046/image_14_1_riikm8.png"
+            alt="Typescript"
+          />
+          <img
+            className="max-h-[3em] w-full object-contain col-span-1"
+            src="https://res.cloudinary.com/dho2pmur7/image/upload/c_scale,w_300/v1682843188/PHP-logo.svg_1_osg7q3.png"
+            alt="PHP"
+          />
+          <img
+            className="max-h-[3em] w-full object-contain col-span-1"
+            src="https://res.cloudinary.com/dho2pmur7/image/upload/c_scale,w_300/v1682845481/image_16_1_nibbjo.png"
+            alt="C#"
+          />
+          <img
+            className="max-h-[3em] w-full object-contain col-span-1"
+            src="https://res.cloudinary.com/dho2pmur7/image/upload/c_scale,w_300/v1682841501/image_8_ow9j2c.png"
+            alt="Python"
+          />
+          <img
+            className="max-h-[3em] w-full object-contain col-span-1"
+            src="https://res.cloudinary.com/dho2pmur7/image/upload/c_scale,w_300/v1682841282/image_5_zryc0v.png"
+            alt="Tailwind"
+          />
+          <img
+            className="max-h-[3em] w-full object-contain col-span-1"
+            src="https://res.cloudinary.com/dho2pmur7/image/upload/c_scale,w_300/v1682840617/image_3_nrk8jx.png"
+            alt="VueJs"
+          />
+          <Image
+            className="max-h-[3em] w-full object-contain col-span-1 dark:invert"
+            src="/images/docker.jpeg"
+            alt="AWS"
+            width="158" height="48"
+          />
+        </div>
+        <div className="mx-auto mt-[2em] grid max-w-[20em] grid-cols-4 items-center gap-x-[1em] gap-y-[1em] sm:max-w-[24em]  sm:gap-x-[1.5em] lg:mx-0 lg:max-w-none sm:grid-cols-8">
+          <img
+            className="max-h-[3em] w-full object-contain col-span-1"
+            src="https://res.cloudinary.com/dho2pmur7/image/upload/c_scale,w_300/v1682841142/image_4_vyfc0e.png"
+            alt="NuxtJs"
+          />
+          <img
+            className="max-h-[3em] w-full object-contain col-span-1"
+            src="https://res.cloudinary.com/dho2pmur7/image/upload/c_scale,w_300/v1682841744/image_10_zfv0fz.png"
+            alt="NodeJs"
+          />
+          <Image
+            className="max-h-[3em] w-full object-contain col-span-1"
+            src="/images/aws.png"
+            alt="AWS"
+            width="158" height="48"
+          />
+          <img
+            className="max-h-[3em] w-full object-contain col-span-1"
+            src="https://res.cloudinary.com/dho2pmur7/image/upload/c_scale,w_300/v1682843277/image_15_1_mxtpes.png"
+            alt=".NET Core"
+          />
+          <img
+            className="max-h-[3em] w-full object-contain col-span-1"
+            src="https://res.cloudinary.com/dho2pmur7/image/upload/c_scale,w_300/v1682843518/mysql-logo-png-transparent_1_uvb3gb.png"
+            alt="MySQL"
+          />
+          <img
+            className="max-h-[3em] w-full object-contain col-span-1"
+            src="https://res.cloudinary.com/dho2pmur7/image/upload/c_scale,w_300/v1682844385/microsoft-sql-server-logo-svgrepo-com_2_mtucyf.png"
+            alt="MsSQL"
+          />
+          <img
+            className="max-h-[3em] w-full object-contain col-span-1"
+            src="https://res.cloudinary.com/dho2pmur7/image/upload/c_scale,w_300/v1682842207/Microsoft_Azure_Logo.svg_1_pwbd5z.png"
+            alt="Azure"
+          />
+          <img
+            className="max-h-[3em] w-full object-contain col-span-1"
+            src="https://res.cloudinary.com/dho2pmur7/image/upload/c_scale,w_300/v1682843441/Ubuntu-Logo_1_ppdvx1.png"
+            alt="Ubuntu"
+          />
+        </div>
+      </div>)}
+  </div>
   </div>
     {/* <div className="grid overflow-hidden grid-cols-3 sm:grid-cols-3 md:grid-cols-3 md:w-1/2 lg:flex lg:flex-col lg:justify-center lg:space-y-4 [&>*]:relative [&>*]:p-6 [&>*]:before:absolute [&>*]:before:bg-gray-800 [&>*]:before:[block-size:100vh] [&>*]:before:[inline-size:1px] [&>*]:before:[inset-block-start:0] [&>*]:before:[inset-inline-start:-1px] [&>*]:after:absolute [&>*]:after:bg-gray-800 [&>*]:after:[block-size:1px] [&>*]:after:[inline-size:100vw] [&>*]:after:[inset-block-start:-1px] [&>*]:after:[inset-inline-start:0] md:[&>*]:p-10">
       <article>
