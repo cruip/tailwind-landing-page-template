f2964e8249761ee1a35c596fc0d852ce6eb961a3
namtrhg <namtrhg@gmail.com>
2024-11-06 15:20:31 +0700

    feat: update

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,11 +1,10 @@
 export const metadata = {
-  title: "Home - Simple",
-  description: "Page description",
+  title: "DongA Corp",
+  description: "Sự hài lòng của khách hàng là ưu tiên hàng đầu của chúng tôi description",
 };
 
 import Hero from "@/components/hero-home";
 import FeaturesPlanet from "@/components/features-planet";
-import LargeTestimonial from "@/components/large-testimonial";
 import Cta from "@/components/cta";
 
 export default function Home() {
